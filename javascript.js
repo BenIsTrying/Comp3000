0eea03dc24803fede641be0519a07af4cee5a1a5
BenIsTrying <benjamin.george-3@students.plymouth.ac.uk>
2023-10-04 20:03:52 +0100

    added login to the webpages with basic JS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
+/*
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "yourusername",
   password: "yourpassword"
+});
+*/
+$(function() {
+    $("#submit").click(function login(){
+        
+        let username = $("#username").val();
+        console.log(username);
+        let password = $("#password").val();
+
+        if ((username = "test") && (password = "password")){
+            console.log("hooray youve logged in");
+        };
+
+    });
+
 });
